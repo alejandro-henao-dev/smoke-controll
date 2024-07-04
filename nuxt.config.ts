4dca894bc9b6a073f5132776d12328c8fbac4ccb
Alejandro Henao <ahenaoes@gmail.com>
2024-07-03 21:32:31 -0500

    add manifest sheenshots and fix icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,29 +15,53 @@ export default defineNuxtConfig({
       name: "Smoke Control",
       short_name: "Smoke Control",
       description: "Control your smoking habits",
+      display: 'standalone',
+      theme_color: '#000000',
+      background_color: '#ffffff',
       icons: [
         {
           src: "/icons/icon_64x64.svg",
           sizes: "64x64",
-          type:"image/svg+xml"
+          type: "image/svg+xml",
+          purpose:"any"
         },
         {
           src: "/icons/icon_144x144.svg",
           sizes: "144x144",
-          type:"image/svg+xml"
+          type: "image/svg+xml",
+          purpose:"any"
         },
         {
           src: "/icons/icon_192x192.svg",
           sizes: "192x192",
-          type:"image/svg+xml"
+          type: "image/svg+xml",
+          purpose:"any"
         },
         {
           src: "/icons/icon_512x512.svg",
           sizes: "512x512",
-          type:"image/svg+xml"
+          type: "image/svg+xml",
+          purpose:"any"
+        }
+      ],
+      screenshots: [    
+        {
+          "src": "/screenshots/screenshot_385x430.png",
+          "sizes": "385x530",
+          "type": "image/png",
+          "form_factor": "narrow",
+          "label": "Application"
+        },
+        {
+          "src": "/screenshots/screenshot_573x352.png",
+          "sizes": "573x352",
+          "type": "image/png",
+          "form_factor": "wide",
+          "label": "Application"
         }
       ]
     },
+    
     workbox: {
       navigateFallback: "/",
       clientsClaim: true,
