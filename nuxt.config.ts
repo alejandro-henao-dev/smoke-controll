45ec43333c069eb47fc6bed4dc6005eda143611d
Alejandro Henao <ahenaoes@gmail.com>
2024-07-03 20:02:53 -0500

    add deployment action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+let development = process.env.NODE_ENV !== 'production'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr:false,
   modules: ["@nuxtjs/tailwindcss"],
   plugins: [
     '~/plugins/jsstore'
-  ]
+  ],
+  app: {
+    baseURL:development ? "/" :"/.output/public"
+  }
 })
